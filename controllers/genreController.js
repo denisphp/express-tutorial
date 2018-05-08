03352b9de393595bfbc239a549075fc5bbed57b2
nabatov <dnabatv@rambler.ru>
2018-05-08 10:12:27 +0300

    add genre view

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,6 +1,8 @@
-var Genre = require('../models/genre');
+var Genre = require('../models/genre'),
+    Book = require('../models/book'),
+    async = require('async');
 
-// Display list of all Genre.
+
 exports.genre_list = function (req, res) {
     Genre.find()
         .sort([['name', 'ascending']])
@@ -15,7 +17,29 @@ exports.genre_list = function (req, res) {
 
 // Display detail page for a specific Genre.
 exports.genre_detail = function (req, res) {
-    res.send('NOT IMPLEMENTED: Genre detail: ' + req.params.id);
+    async.parallel({
+        genre: function (callback) {
+            Genre.findById(req.params.id)
+                .exec(callback);
+        },
+
+        genreBooks: function (callback) {
+            Book.find({'genre': req.params.id})
+                .exec(callback);
+        },
+
+    }, function (err, results) {
+        if (err) {
+            return next(err);
+        }
+        if (results.genre == null) { // No results.
+            var err = new Error('Genre not found');
+            err.status = 404;
+            return next(err);
+        }
+
+        res.render('genres/view', {title: 'Genre Detail', genre: results.genre, genreBooks: results.genreBooks});
+    });
 };
 
 // Display Genre create form on GET.
